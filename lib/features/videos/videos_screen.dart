fd827c6e78d93e4f801bb52d8b3a49150a36e04f
ethan <noahkim.j@gmail.com>
2024-02-19 20:04:21 +0900

    #5.4 AnimatedCrossFade

---
+++ lib/features/videos/videos_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class VideosScreen extends StatelessWidget {
+  const VideosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
