cb4027782adebe51dd612cc36551f5bfe7e9a74e
ethan <noahkim.j@gmail.com>
2024-02-19 21:07:00 +0900

    #6.1 pushAndRemoveUntil

--- lib/features/onboarding/tutorial_sceen.dart
+++ lib/features/onboarding/tutorial_sceen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
+import '../../main_navigation/main_navigation_screen.dart';
 
 enum Direction { right, left }
 
@@ -40,6 +41,15 @@ class _TutorialScreenState extends State<TutorialScreen> {
         _showingPage = Page.first;
       });
     }
+  }
+
+  void _onEnterAppTap() {
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (context) => const MainNavigationScreen(),
+      ),
+      (route) => false,
+    );
   }
 
   @override
@@ -107,7 +117,7 @@ class _TutorialScreenState extends State<TutorialScreen> {
                 duration: const Duration(milliseconds: 300),
                 opacity: _showingPage == Page.first ? 0 : 1,
                 child: CupertinoButton(
-                  onPressed: () {},
+                  onPressed: _onEnterAppTap,
                   color: Theme.of(context).primaryColor,
                   child: const Text('Enter the app!'),
                 ),
---
+++ lib/main_navigation/main_navigation_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainNavigationScreen extends StatelessWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
