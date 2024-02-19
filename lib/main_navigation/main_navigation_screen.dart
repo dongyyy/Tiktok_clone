b3e6c06536e4de5088ccc7aec659a26eccada15b
ethan <noahkim.j@gmail.com>
2024-02-19 21:23:53 +0900

    #6.2 BottomNavigationBar

--- lib/main_navigation/main_navigation_screen.dart
+++ lib/main_navigation/main_navigation_screen.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  var screens = [
+    Center(
+      child: Text("First"),
+    ),
+    Center(
+      child: Text("Second"),
+    )
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTap(int selectedIndex) {
+    setState(() {
+      _selectedIndex = selectedIndex;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar( // 1. BottomNavigationBar 사용
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          onTap: _onTap,
+          //selectedItemColor: Theme.of(context).primaryColor,
+          items: const [
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: "Home",
+              tooltip: "What are you?",
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+              label: "Search",
+              tooltip: "What are you?",
+              backgroundColor: Colors.blue,
+            )
+          ],
+      ),
+    );
   }
 }
