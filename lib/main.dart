f00d358cb27107dca8f8692f6ae39e278dbc8a35
ethan <noahkim.j@gmail.com>
2024-02-21 22:56:51 +0900

    #6.5 Custom NavigationBar
    #26.6 Conclusions (08:37)
    #27 VIDEO TIMELINE
    
    #27.0 Introduction (01:40)
    
    #27.1 VideoPost (17:26)
    
    #27.2 Infinite Firebase (15:53)
    
    #27.3 FamilyAsyncNotifier (14:20)
    
    #27.4 likeVideo (07:09)
    
    #27.5 dislikeVideo (08:18)
    
    #27.6 Code Challenge (03:16)
    #28 DIRECT MESSAGES
    
    #28.0 Introduction (03:24)
    
    #28.1 Send Message (15:41)
    
    #28.2 StreamProvider (09:05)
    
    #28.3 autoDispose (14:05)
    
    #28.4 Code Challenge (05:12)
    #29 PUSH NOTIFICATIONS
    
    #29.0 Introduction (03:45)
    
    #29.1 getToken (07:04)
    
    #29.2 Foreground Notifications (06:44)
    
    #29.3 Background Notification (09:50)
    
    #29.4 ShellRoute (11:14)
    
    #29.5 Targeted Notification (09:21)
    #30 SECURITY AND TESTING
    
    #30.0 Introduction (02:04)
    
    #30.1 Security Rules (10:50)
    
    #30.2 Security Querying (10:18)
    
    #30.3 Unit Testing (11:30)
    
    #30.4 Widget Testing part One (12:03)
    
    #30.5 Widget Testing part Two (11:22)
    
    #30.6 Integration Testing part One (13:28)
    
    #30.7 Integration Testing part Two (10:28)
    
    #30.8 Conclusions (01:44)
    #6.6 Stateful Navigation part One

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone_dy/features/onboarding/interests_screen.dart';
+
+import 'constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -12,9 +13,23 @@ class TikTokApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      title: 'TikTok Clone',
-      home: MainNavigationScreen(),
-    );
+    return MaterialApp(
+        title: 'TikTok Clone',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFE9435A),
+        ),
+        home: const InterestsScreen());
   }
 }
